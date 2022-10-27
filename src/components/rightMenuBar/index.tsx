5dd89f1bd034fc5e15441b4532c92a5fc45efe30
Bruno Alves Vieira <brunnooa.v@gmail.com>
2022-10-27 20:56:14 -0300

    c

--- src/components/rightMenuBar/index.tsx
+++ src/components/rightMenuBar/index.tsx
@@ -1,10 +1,26 @@
 import { RigthMenuBarStyled } from "./styled"
 import { BurguerMenu } from "./BurgerMenu"
+import { useState } from "react"
 
 export const RigthMenuBar = () =>{
+
+    //Texts
+    const [cover, setCover] = useState('Cover')
+    const [about, setAbout] = useState('About')
+    const [experience, setExperience] = useState('Experience')
+    const [skills, setSkills] = useState('Skills')
+    const [projects, setProjects] = useState('Projects')
+
     return (
         <RigthMenuBarStyled>
             <BurguerMenu/>
+            <ul>
+                <a href=""><li>{cover}</li></a>
+                <a href=""><li>{about}</li></a>
+                <a href=""><li>{experience}</li></a>
+                <a href=""><li>{skills}</li></a>
+                <a href=""><li>{projects}</li></a>
+            </ul>
         </RigthMenuBarStyled>
     )
 }
--- src/components/rightMenuBar/styled.tsx
+++ src/components/rightMenuBar/styled.tsx
@@ -1,6 +1,27 @@
 import styled from "styled-components";
 
 export const RigthMenuBarStyled = styled.div`
+    @import url('https://fonts.googleapis.com/css2?family=Dosis:wght@700&display=swap');
 
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 1;
+    background-color: #7199FF;
+    height: 100vh;
+    width: 300px;
+    border: 1px solid #ffff;
+    box-shadow: 1px 1px 1px black;
 
+    ul {
+        height: 80%;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        font-family: 'Dosis', sans-serif;
+        font-size: 32px;
+        a {
+            color: #ffff;
+        }
+    }
 `
