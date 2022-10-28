067a737b4a1df76e5eea547aebce76d029452a49
Bruno Alves Vieira <brunnooa.v@gmail.com>
2022-10-28 20:46:35 -0300

    c

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { ContextProvider } from './contexts/Context'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <ContextProvider>
-      			<App />
-		</ContextProvider>
+    <App />
   </React.StrictMode>
 )
