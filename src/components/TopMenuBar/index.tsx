b8c4366b68fa574a919acfa8be97809d70996c0c
Bruno Alves Vieira <brunnooa.v@gmail.com>
2022-11-01 19:23:04 -0300

    c

--- src/components/TopMenuBar/index.tsx
+++ src/components/TopMenuBar/index.tsx
@@ -1,11 +1,18 @@
+import { useContext } from "react"
+import { Context } from "../../contexts/Context"
 import { TopMenuBarStyled } from "./styled"
 
 export const TopMenuBar = () => {
+    const {state, dispatch} = useContext(Context)
+
+    const changeTheme = () => {
+        dispatch
+    }
 
     return (
         <TopMenuBarStyled>
             <img src="TopMenuBar/BrazilLogo.svg" alt="" className="languageBTN" />
-            <img src="TopMenuBar/Sun.svg" alt="" className="themeBTN" />
+            <img src="TopMenuBar/Sun.svg" alt="" className="themeBTN" onClick={changeTheme}/>
         </TopMenuBarStyled>
     )
 }
