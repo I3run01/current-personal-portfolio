733d22a66e15a2e2773f04db594f9610e50cf96d
Bruno Alves Vieira <brunnooa.v@gmail.com>
2022-11-04 19:23:49 -0300

    c

--- src/components/leftMenuBar/index.tsx
+++ src/components/leftMenuBar/index.tsx
@@ -1,21 +1,39 @@
 import { LeftMenuBarStyled } from "./styled"
 import { BurguerMenu } from "./BurgerMenu"
-import { useState } from "react"
+import { useContext, useEffect, useState } from "react"
+import { Context } from "../../contexts/Context"
 
 export const LeftMenuBar = () =>{
-
+    const {state, dispatch} = useContext(Context)
     const [menuOpen, setMenuOpen] = useState<string>('closed')
+
+    //Texts
+    const [cover, setCover] = useState<string>(state.language.status === 'english' ? 'Cover' : 'Capa')
+    const [about, setAbout] = useState<string>(state.language.status === 'english' ? 'About' : 'Sobre')
+    const [Experiences, setExperiences] = useState<string>(state.language.status === 'english' ? 'Experiences' : 'Experiências')
+    const [skills, setSkills] = useState<string>(state.language.status === 'english' ? 'Skills' : 'Habilidades')
+    const [projects, setProjects] = useState<string>(state.language.status === 'english' ? 'Projects' : 'Projetos')
+
+    useEffect(() => {
+        if(state.language.status === 'english') {
+            setCover('Cover')
+            setAbout('About')
+            setExperiences('Experiences')
+            setSkills('Skills')
+            setProjects('Projects')
+        } else {
+            setCover('Capa')
+            setAbout('Sobre')
+            setExperiences('Experiências')
+            setSkills('Habilidades')
+            setProjects('Projetos')
+        }
+    }, [state.language.status])
 
     const handleMenuButton = () => {
         menuOpen === 'opened' ? setMenuOpen('closed') :  setMenuOpen('opened')
     }
 
-    //Texts
-    const [cover, setCover] = useState('Cover')
-    const [about, setAbout] = useState('About')
-    const [experience, setExperience] = useState('Experience')
-    const [skills, setSkills] = useState('Skills')
-    const [projects, setProjects] = useState('Projects')
 
     return (
         <LeftMenuBarStyled className={menuOpen}>
@@ -23,7 +41,7 @@ export const LeftMenuBar = () =>{
                 <BurguerMenu fction={handleMenuButton}/>    
                 <a href="#CoverIndex"> · {cover}</a>
                 <a href="#AboutIndex"> · {about}</a>
-                <a href="#"> · {experience}</a>
+                <a href="#"> · {Experiences}</a>
                 <a href="#"> · {skills}</a>
                 <a href="#"> · {projects}</a>
             </div>
