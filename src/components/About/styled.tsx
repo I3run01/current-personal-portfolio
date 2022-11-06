9a8c29692bf47fa65c139b601601e5d637e50d1b
I3run01 <brunnooa.v@gmail.com>
2022-11-06 08:31:26 -0300

    c

--- src/components/About/styled.tsx
+++ src/components/About/styled.tsx
@@ -9,6 +9,7 @@ const titleLightColor = '#000000'
 
 export const AboutStyled = styled.div`
     @import url('https://fonts.googleapis.com/css2?family=Dosis:wght@700&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap');
 
     min-height: 100vh;
 
@@ -30,6 +31,7 @@ export const AboutStyled = styled.div`
         border: 4px solid ${props => props.theme === 'dark' ? titleDarkColor : titleLightColor};
         background-color: rgba(0, 57, 204, 20%);
         text-align: center;
+        font-family: ope;
 
         p {
             padding: 10px;
@@ -43,10 +45,13 @@ export const AboutStyled = styled.div`
 
         #text {
             width: auto;
-            margin-left: 20vw;
+            margin-left: 10vw;
             margin-top: -40px;
 
             p {
+                font-family: 'Open Sans', sans-serif;
+                line-height: 28px;
+                font-size: 24px;
                 padding-top: 20px;
             }
         }
--- src/components/Experiences/styled.tsx
+++ src/components/Experiences/styled.tsx
@@ -9,11 +9,30 @@ const titleLightColor = '#000000'
 
 export const ExperiencesStyled = styled.div`
     @import url('https://fonts.googleapis.com/css2?family=Dosis:wght@700&display=swap');
+    @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap');
+
+    h1, h2, h3 {
+        font-family: 'Dosis', sans-serif;
+        color: ${props => props.theme === 'dark' ? titleDarkColor : titleLightColor};
+    }
 
     h1 {
         margin-left: 0;
         font-size: 48px;
-        font-family: 'Dosis', sans-serif;
+    }
+
+    h2 {
+        font-size: 36px;
+    }
+
+    h3 {
+        font-size: 24px;
+    }
+
+    p {
+        line-height: 28px;
+        font-size: 16px;
+        font-family: 'Open Sans', sans-serif;
         color: ${props => props.theme === 'dark' ? titleDarkColor : titleLightColor};
     }
 
