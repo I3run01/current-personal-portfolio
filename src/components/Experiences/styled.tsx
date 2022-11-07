8f55eac814d8f3c24497de60aa415fa463864771
I3run01 <brunnooa.v@gmail.com>
2022-11-07 08:34:25 -0300

    c

--- src/components/Experiences/styled.tsx
+++ src/components/Experiences/styled.tsx
@@ -11,6 +11,8 @@ export const ExperiencesStyled = styled.div`
     @import url('https://fonts.googleapis.com/css2?family=Dosis:wght@700&display=swap');
     @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap');
 
+    margin-top: 100px;
+
     h1, h2, h3 {
         font-family: 'Dosis', sans-serif;
         color: ${props => props.theme === 'dark' ? titleDarkColor : titleLightColor};
