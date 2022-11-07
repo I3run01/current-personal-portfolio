a94b729b8496520506608fe78358da368d8826c6
Bruno Alves Vieira <brunnooa.v@gmail.com>
2022-11-07 18:48:19 -0300

    c

--- src/components/Cover/Screen/style.tsx
+++ src/components/Cover/Screen/style.tsx
@@ -9,12 +9,24 @@ const lightColor = '#000000'
 
 export const ScreenStyle = styled.div<Props>`
     display: flex;
+    position: relative;
     z-index: 1;
 
     width: 385px;
-    background-color: red;
-    border: 10px solid ${props => props.theme === 'dark' ? darkColor : lightColor};
+    border: 7px solid ${props => props.theme === 'dark' ? darkColor : lightColor};
     border-radius: 40px;
+    overflow: hidden;
+    -webkit-overflow-scrolling: auto;
+    box-shadow: 1px 1px 1px black;
+    background-color: ${props => props.theme === 'dark' ? '#00000017' : '#ffffff25f'};
+
+    img {
+        margin-top: -3px;
+        transform: translateX(-50%);
+        position: absolute;
+        height: 105%;
+        left: 50%;
+    }
 
     ::before {
         content: '';
@@ -24,17 +36,25 @@ export const ScreenStyle = styled.div<Props>`
     }
 
     #bkLeft, #bkRight {
+        margin-top: -10px;
+        margin-left: -10px;
+        margin-right: -10px;
+        padding: 0;
         z-index: 0;
-        height: 100%;
-        width: 50%;
+        height: 110%;
+        width: 55%;
     }
 
     #bkLeft {
-        background-color: green;
+        background: rgb(0,255,139);
+        background: linear-gradient(215deg, rgba(0,255,139,0.6) 0%, rgba(145,8,178,0.6) 100%);
+        opacity: 80%;
     }
 
     #bkRight {
-        background-color: yellow;
+        background: rgb(0,71,255);
+        background: linear-gradient(135deg, rgba(0,71,255,0.6) 0%, rgba(217,140,20,6) 100%);
+        opacity: 80%;
     }
     
 `
