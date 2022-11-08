25ca937c66a98c95627113405b29215418aa8e62
I3run01 <brunnooa.v@gmail.com>
2022-11-08 08:42:25 -0300

    c

--- src/components/Cover/index.tsx
+++ src/components/Cover/index.tsx
@@ -1,24 +1,7 @@
-import { useContext, useEffect, useState } from "react"
-import { Context } from "../../contexts/Context"
 import { CoverStyled } from "./styled"
 import { Screen } from "./Screen"
 
 export const Cover = () => {
-    const {state, dispatch} = useContext(Context)
-    
-    const screenLightEng = 'Cover/ScreenLightEng.svg'
-    const screenDarkEng = 'Cover/ScreenDarkEng.svg'
-    const screenLightPTBR = 'Cover/ScreenLightPTBR.svg'
-    const screenDarkPTBR = 'Cover/ScreenDarkPTBR.svg'
-
-    const [coverImage, setCoverImage] = useState<string>(state.theme.status === 'dark' ? screenLightEng : screenDarkEng)
-
-    useEffect(() => {
-        if(state.theme.status === 'light' && state.language.status === 'english') setCoverImage(screenLightEng)
-        else if(state.theme.status === 'light' && state.language.status === 'portuguese') setCoverImage(screenLightPTBR)
-        else if(state.theme.status === 'dark' && state.language.status === 'portuguese') setCoverImage(screenDarkPTBR)
-        else setCoverImage(screenDarkEng)
-    }, [ ,state.theme.status, state.language.status])
 
     return (
         <CoverStyled
