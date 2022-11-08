3db86961e86c09776b89cde95baa3dcc649cc7d2
Bruno Alves Vieira <brunnooa.v@gmail.com>
2022-11-08 19:58:53 -0300

    c

--- src/components/Projects/styled.tsx
+++ src/components/Projects/styled.tsx
@@ -14,14 +14,43 @@ export const ProjectsStyles = styled.div`
 
     padding-bottom: 100px;
 
-    h1 {
-        font-size: 48px;
+    h1, h2 { 
         font-family: 'Dosis', sans-serif;
         color: ${props => props.theme === 'dark' ? titleDarkColor : titleLightColor}; 
     }
 
+    h1 {
+        font-size: 48px;
+    }
+
+    h2 {
+        font-size: 24px;
+        z-index: +1;
+        background-color: purple;
+    }
+
     #projectsTypes {
+        position: relative;
+
         width: 100%;
+        overflow: hidden;
+        border: 2px solid ${props => props.theme === 'dark' ? titleDarkColor : titleLightColor};
+
+        ::before {
+            content: '';
+            height: 0;
+            float: left;
+            padding-bottom: 32%;
+        }
+
+        img {
+            position: absolute;
+            top: 0;
+            left: 0;
+            z-index: 0;
+            opacity: 0;
+        }
+
     }
     
 
