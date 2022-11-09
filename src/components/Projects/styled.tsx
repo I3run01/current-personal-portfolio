03d254bc1a34c0cd569f9abe289f87cf76c075ff
Bruno Alves Vieira <brunnooa.v@gmail.com>
2022-11-09 15:44:45 -0300

    Tested project

--- src/components/Projects/styled.tsx
+++ src/components/Projects/styled.tsx
@@ -8,7 +8,7 @@ const titleDarkColor = '#ffff'
 const titleLightColor = '#000000'
 
 
-export const ProjectsStyles = styled.div`
+export const ProjectsStyles = styled.div<Props>`
     @import url('https://fonts.googleapis.com/css2?family=Dosis:wght@700&display=swap');
     @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap');
 
