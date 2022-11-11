64b685bbdcc5f6e86ac223e3017378a20e4870d2
I3run01 <brunnooa.v@gmail.com>
2022-11-11 09:29:49 -0300

    c

--- src/pages/landingPages.tsx
+++ src/pages/landingPages.tsx
@@ -3,8 +3,9 @@ import {ProjectsDetails} from '../components/ProjectsDetails'
 export const LandingPages = () => {
 
     return (
-        <ProjectsDetails>
-            <h2>Landing Page</h2>
-        </ProjectsDetails>
+        <div>
+             <ProjectsDetails/>
+        </div>
+       
     )
 }
