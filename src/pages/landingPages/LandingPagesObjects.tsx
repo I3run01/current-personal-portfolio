6c048a95a61b2e03d2d5b7d3c017741bf28ef177
Bruno Alves Vieira <brunnooa.v@gmail.com>
2022-11-11 19:39:09 -0300

    c

---
+++ src/pages/landingPages/LandingPagesObjects.tsx
@@ -0,0 +1,40 @@
+import {projectDetailsClass} from '../../components/ProjectsDetails/ProjectDetailsClass/ProjectDetailsClass'
+
+export const landingPages = [
+    new projectDetailsClass
+    (
+        'Classic Portfolio',
+        'Portifólio',
+        "Realizei esse projeto em React.JS, nesse projeto eu aprendi a fazer animações com CSS, trabalhei com variáveis globais (ContextAPI) e componentes",
+        " I did this project with React.JS, I used ContextAPI to work with global variables, I also worked with components and CSS animations.",
+        'ProjectsDetails/landingPages/Cape.png',
+        [{
+            icons: 'Icons/GitHub.png',
+            name: 'GitHub',
+            link: 'https://github.com/I3run01/Portfolio-Project-V-1.0'
+        }],
+        [
+            {
+               image: 'ProjectsDetails/landingPages/Gallary/About Me.png',
+               textENG: 'Page that say about me',
+               textPTBR: 'Página que fala sobre mim'
+            },
+            {
+                image: 'ProjectsDetails/landingPages/Gallary/Dark theme.png',
+                textENG: 'Dark theme in the site',
+                textPTBR: 'Tema escuro do site'
+             },
+             {
+                image: 'ProjectsDetails/landingPages/Gallary/Projects.png',
+                textENG: "This part of my old portfolio shows the projects I've done",
+                textPTBR: 'Essa parte do site mostra os projetos que fiz'
+             },
+             {
+                image: 'ProjectsDetails/landingPages/Gallary/Skills.png',
+                textENG: "This part of my old portfolio shows the skills that I have",
+                textPTBR: 'Essa parte do site mostra as habilidades que tenho'
+             },
+        ]
+
+    )
+]
