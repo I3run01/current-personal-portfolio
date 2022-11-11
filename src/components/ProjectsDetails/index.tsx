161442fa87fcf708bc69065f09e6692a6535ed63
I3run01 <brunnooa.v@gmail.com>
2022-11-11 18:21:14 -0300

    c

--- src/components/ProjectsDetails/index.tsx
+++ src/components/ProjectsDetails/index.tsx
@@ -1,7 +1,17 @@
 import { ProjectsDetailsStyles } from './styled'
 import { MainStyled } from '../../globasStyles'
 
-export const ProjectsDetails = () => {
+export const ProjectsDetails = 
+    (
+        categoryPTBR: string,
+        categoryENG: string,
+        titlePTBR: string,
+        titleENG:string,
+        textPTBR:string,
+        textENG:string,
+        
+
+    ) => {
     return (
         <MainStyled>
             <ProjectsDetailsStyles>
