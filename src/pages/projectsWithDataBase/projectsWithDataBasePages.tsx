b5a297036bf8295097a80607838421ebe82d988f
Bruno Alves Vieira <brunnooa.v@gmail.com>
2022-11-18 18:09:08 -0300

    c

--- src/pages/projectsWithDataBase/projectsWithDataBasePages.tsx
+++ src/pages/projectsWithDataBase/projectsWithDataBasePages.tsx
@@ -6,8 +6,8 @@ export const ProjectsWithDataBasePage = () => {
     return (
         <div>
              <ProjectsDetails
-             categoryPTBR='projetos com banco de dados'
-             categoryENG='projects with database'
+             categoryPTBR='Projetos com banco de dados'
+             categoryENG='Projects with database'
              projects={projectsWithDataBase}
              />
         </div>
